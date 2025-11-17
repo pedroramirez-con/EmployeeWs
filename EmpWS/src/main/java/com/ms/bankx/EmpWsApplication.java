84a349e89d63f945dcb590e287d499cd6d9a9c64
PEDRO RAMIREZ CHAVEZ <pedro.ramirez+oxxogh@consiss.cloud>
2025-11-17 01:17:13 -0600

    Primer commit

---
+++ EmpWS/src/main/java/com/ms/bankx/EmpWsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.bankx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpWsApplication.class, args);
+	}
+
+}
